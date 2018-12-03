a2868c4afbee0ee38ea1f39df541a9f17c041b67
malashenkoEvgeniy <malashenko_evgeniy@mail.ua>
2018-12-03 22:24:07 +0200

    добавляет функцию подсчета количества часов до наступления даты в параметре

--- data.php
+++ data.php
@@ -4,25 +4,25 @@ $projects = ['Входящие', 'Учеба', 'Работа', 'Домашние
 $tasks = [
     [
         'task' => '<b style="color: red;">Собеседование</b> в IT компании',
-        'date' => '01.06.2018',
+        'date' => '22.12.2018',
         'project' => $projects[2],
         'status' => false
     ],
     [
         'task' => 'Выполнить тестовое задание',
-        'date' => '25.05.2018',
+        'date' => '25.12.2018',
         'project' => $projects[2],
         'status' => false
     ],
     [
         'task' => 'Сделать задание первого раздела',
-        'date' => '21.04.2018',
+        'date' => '21.12.2018',
         'project' => $projects[1],
         'status' => true
     ],
     [
         'task' => 'Встреча с другом',
-        'date' => '22.04.2018',
+        'date' => '22.12.2018',
         'project' => $projects[0],
         'status' => false
     ],
